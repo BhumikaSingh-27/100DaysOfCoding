a9ffd95a666ba26ea1127aec4924bcae0eba2f99
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-10 08:33:52 +0530

    undefined keyword
    examples of undefined and not defined

---
+++ Day5-Prototypes/app.js
@@ -0,0 +1,13 @@
+//DAY 5
+// difference between undefined and not defined
+//JS loosely/ weakly coupled language, we don't have to specifically define the type of variable.
+//undefined is the label/placeholder which is assigned before the execution of code.
+//not defined variables are the ones which are not declared or has not be given memory space.
+// console.log(a) //undefined
+// var a = "bhumika"
+
+// console.log(a) //bhumika
+// console.log(b) //error: b is not defined!
+
+
+//Prototypes
