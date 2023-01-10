1f2f3481190eafeb2a713f4c65e23931d6573a4c
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-10 10:07:47 +0530

    Object Prototype
    examples to demonstrate prototyping and inheritance concepts in JS

--- Day5-Prototypes/app.js
+++ Day5-Prototypes/app.js
@@ -10,4 +10,41 @@
 // console.log(b) //error: b is not defined!
 
 
-//Prototypes
+//Prototypes in JS
+// mechanism by which one object inherits the properties of another object.
+//In JS, every object has built-in properties called its prototype. This itself an object and it will have its own buit-in propertied/protoype. This called prototype chaining.
+
+var person ={
+    name:"bhumi",
+    greet(){
+        console.log("hello")
+    }
+}
+console.log(person) // gives build-in methods/prototype of object
+
+var obj1 = {
+    name:"Bhumika",
+    getName: function(){
+        return this.name;
+    }
+}
+
+//to pass an object as the prototype we use __proto__ 
+var obj2={
+    rollno:3,
+    name:"Singh",
+    __proto__:obj1
+}
+
+console.log(obj2)
+console.log(obj2.name) 
+console.log(obj2.getName()) //this will point to the current object, if name not present,it will use prototype object
+
+//inheritance in js
+ var obj3={
+    section:"A",
+    __proto__:obj2
+ }
+ 
+console.log(obj3)
+console.log(obj3.getName())
