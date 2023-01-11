116337ab98a0270341a5f2c5371f16350b74d5f9
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-11 15:17:33 +0530

    Set
    added examples and methods of set

--- Day6-SetWeakMap/app.js
+++ Day6-SetWeakMap/app.js
@@ -2,4 +2,40 @@
 // Map Set WeakMap WeakSet
 
 //difference in array and set
-//
+//set contains unique value but array can have duplicate values
+// ============Definition========
+// Set objects are a collection of values which stores unique value of any typeof.
+// it can be iterated in the order of insertion.
+
+const mySet = new Set();
+console.log(mySet);
+
+const myArray = [1, 2, 3, 4];
+const set = new Set(myArray);
+console.log(set);
+//add value to set
+set.add(4);
+console.log(set.size);
+//delete a value
+set.delete(4);
+//store object in set
+console.log(set.add({ name: "bhumika" }));
+
+//to clear set object
+set.clear();
+console.log(set);
+
+//to find/check for an element in set
+console.log(set.has(2)); //true or false
+
+//iterate over set for or forEach
+for (let item of set) {
+  console.log(item);
+}
+set.forEach((element) => {
+  console.log(element);
+});
+
+//Map
+//=============Definition==============
+// Map object holds key-value pairs and remembers the original insertion order of the key
