76c7884e2ca7a2a4be2a00e586266803310422a0
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-14 17:04:54 +0530

    Currying Memoize

--- Day8-CurryingMemoize/momoize.js
+++ Day8-CurryingMemoize/momoize.js
@@ -3,5 +3,31 @@
 //Memoization is a technique for speeding up applications by caching the results of expensive function calls and returning them when the same inputs are used again.
 //cache -> object
 
-let sum = 0;
-function calc()
+function calc(n){
+      let sum =0
+      for(let i =0; i<=n;i++){
+        sum+=i
+      }
+    return sum
+    }
+    
+    function memoize(fun){
+      let cache = {}
+      return function(n){
+       if(n in cache){
+        return cache[n]
+       }else{
+        let res = fun(n);
+        cache[n]=res;
+        return res
+       }
+    
+  }
+  console.time();
+  const total = memoize(calc)(5) //currying
+  console.log(total)
+  console.timeEnd();
+  console.log("----------")
+  console.time();
+  console.log(calc(5))
+  console.timeEnd()
