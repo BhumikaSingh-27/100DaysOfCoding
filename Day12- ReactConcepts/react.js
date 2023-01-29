2c88d98dd0f88d6fc2bff83fb14852b7fb791606
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-29 22:54:35 +0530

    initial commit

---
+++ Day12- ReactConcepts/react.js
@@ -0,0 +1,5 @@
+// React
+// is a javascript library to build User Interface. It lets you compose complex UIs from small and isolated pieces of code called “components”.
+//- Declarative
+//-component based
+//- Learn once, write anywhere
