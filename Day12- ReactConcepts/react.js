772f09b0e7a866e390f8573be561a1c79ad20460
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-30 12:18:46 +0530

    docs

--- Day12- ReactConcepts/react.js
+++ Day12- ReactConcepts/react.js
@@ -3,3 +3,19 @@
 //- Declarative
 //-component based
 //- Learn once, write anywhere
+
+// Every thing in react is due to state change. React doen't manipulate all the DOM, rather it creates a virtual copyofDOm, and compares it with actual DOM.
+//Wherever it finds the change, it directly manipulates the DOM, without refreshing the whole page.
+
+//useState - is a function that returns array of state variable and setter function.
+//when the state variable changes then the view changes.
+//view is what the App() function returns.
+// export default function App() {
+//     return (
+//       <div>
+//         <h1>Hello CodeSandbox</h1>
+//         <h2>Start editing to see some magic happen!</h2>
+//       </div>
+//     );
+//   }
+
