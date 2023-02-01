d26c14ffb5a2bc8e2c1e6e760effd5a270c97710
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-02-01 11:16:02 +0530

    JSX

--- Day12- ReactConcepts/react.js
+++ Day12- ReactConcepts/react.js
@@ -57,3 +57,22 @@ export default function App() {
 
 //React has its own DOM - copy of actual DOM called virtual DOM.
 //NOTE: Every time the state changes, the re-renterig happens
+
+//===============JSX================
+// It is the syntax extension to JS.
+//we use to describe our UI.
+//LIKE PUTTING LOGIC AND MARKUP TOGETHER
+//in React, rendering logic and markup live together in the same place—components.
+// JSX is a special way of writing JavaScript. That means it’s possible to use JavaScript inside it—with curly braces { }
+
+// export default function TodoList() {
+//     const name = 'Gregorio Y. Zara';
+//     return (
+//       <h1>{name}'s To Do List</h1>
+//     );
+//   }
+// Rules of JSX:
+
+// 1. Returns one root element - <></>- this empty tag is called fragment. Fragments let you group things without leaving any trace in the browser HTML tree. or you can use <div></div>.
+// 2. Close all tags
+// 3. use CamelCase for attributes - className is used instead of class since it is reserved word in JS.
