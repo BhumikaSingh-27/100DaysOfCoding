e09fb5c9a8da36684527ff9f817c7acd6ac58e55
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-08-27 22:44:02 +0530

    promise

--- Day14-Promises/promise.js
+++ Day14-Promises/promise.js
@@ -14,27 +14,46 @@ console.log("first");
 
 // console.log("end");
 
-function anotherFunction(name) {
-  setTimeout(() => {
-    return `name is ${name}`;
-  }, 2000);
-}
-//this func is async, so it will go to setTimeout API, it will take time, executed at last
-// const message = anotherFunction("Bhumika is the best!")
-// console.log(message) //message is not having data, so undefined
-// console.log("end of it")
+// function anotherFunction(name) {
+//   setTimeout(() => {
+//     return `name is ${name}`;
+//   }, 2000);
+// }
+// //this func is async, so it will go to setTimeout API, it will take time, executed at last
+// // const message = anotherFunction("Bhumika is the best!")
+// // console.log(message) //message is not having data, so undefined
+// // console.log("end of it")
 
-//if we want to print the message after sync code is eecuted then we will use concept of callback
-function anotherFunctionWWithCallback(name, cb) {
-  setTimeout(() => {
-    cb(`name is ${name}`);
-  }, 2000);
-}
+// //if we want to print the message after sync code is eecuted then we will use concept of callback
+// function anotherFunctionWWithCallback(name, cb) {
+//   setTimeout(() => {
+//     cb(`name is ${name}`);
+//   }, 2000);
+// }
 
-anotherFunctionWWithCallback(
-  "Bhumika is the best!",
-  (action) => {
-    console.log(action);
-  }
-);
-console.log("end of it");
+// function likeTheVideo(video, cb) {
+//   setTimeout(() => {
+//     cb(`liked the ${video}`);
+//   }, 2000);
+// }
+
+// anotherFunctionWWithCallback("Bhumika is the best!", (action) => {
+//   console.log(action);
+//   likeTheVideo("bhumika's video", (action) => {
+//     console.log(action); //callback hell, if continued
+//   });
+// });
+// console.log("end of it");
+
+//Promises
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const result = true;
+    if (result) resolve("promise is resolved");
+    else reject(new Error("promise is rejected"));
+  }, 2000);
+});
+
+// promise.catch((err) => console.log(err)).then((res) => console.log(res));
+console.log(promise)
+console.log("end");
