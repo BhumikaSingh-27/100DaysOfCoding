2c987a7c2313736446a1d74678ec826404a292d8
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-09-07 09:43:38 +0530

    remove dup

---
+++ Day18-RemoveDups/app.js
@@ -0,0 +1,14 @@
+//remove duplicates
+
+//using filter
+const arr = [1, 2, 3, 3, 4];
+// const uniqueArray = arr.filter((num, index, arr) => arr.indexOf(num) === index);
+// console.log(uniqueArray);
+
+
+//using set
+// const uniqueArray = [...new Set(arr)]
+// console.log(uniqueArray)
+
+//using reduce
+// const uniqueArray = arr.reduce((acc,val)=>)
