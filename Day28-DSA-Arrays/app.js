7805adfcd26f673877736a6c578d96bed2acb342
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-09-22 22:40:44 +0530

    second largest element in array

--- Day28-DSA-Arrays/app.js
+++ Day28-DSA-Arrays/app.js
@@ -12,3 +12,32 @@ const secondLargest = (arr) => {
 };
 
 console.log(secondLargest([1, 34, 23, 2, 35, 89]));
+
+//to remove duplicates in an array
+const removeDups = (arr) => {
+  const unique = arr.filter((ele,index,arr)=>arr.indexOf(ele) === index)
+  return unique
+}
+console.log(removeDups([1,4,2,2,3,6,4,3]))
+
+
+//find second maximum, my approach :)
+const findSecondMax = (arr) => {
+  const uniqueArray = arr.filter((ele,index,arr)=>arr.indexOf(ele) === index)
+
+  let max=uniqueArray[0]
+  let prevMax = 0
+  for(let i=1;i<=uniqueArray.length;i++){
+    if(arr[i]>max){
+      prevMax= max;
+      max=arr[i]
+    }
+  }
+  return prevMax
+}
+
+console.log(findSecondMax([1,4,2,2,3,6,4,3]))
+
+
+//------------------------------------q2:Rotate Array by K-----------------------
+ 
