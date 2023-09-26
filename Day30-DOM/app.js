e9313f513bad9d8a554414a47cb7db4c7ef99af9
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-09-26 23:03:25 +0530

    DOM selection

---
+++ Day30-DOM/app.js
@@ -0,0 +1,25 @@
+//SELECTING DOM ELEMNTS
+
+//getElementsById
+// const title = document.getElementById("heading");
+
+// console.log(title);
+
+//getElementsByClassName
+// const list = document.getElementsByClassName("list-items");
+// console.log(list);
+
+//getElementsByTagName
+const listItem = document.getElementsByTagName("li");
+console.log(listItem);
+
+//querySelector
+const container = document.querySelector("div");
+console.log(container);
+
+//querySelectorAll
+const listItems = document.querySelectorAll(".list-items");
+console.log(listItems);
+
+const item = document.querySelector(".list-items");
+console.log(item);
--- Day9-Debouncing/app.js
+++ Day9-Debouncing/app.js
@@ -12,11 +12,11 @@ function mydebouncing(fun, d) {
     };
   }
   
-  let counter = 1;
-  const getData = mydebouncing(function () {
-    console.log("debouncing", counter++);
-    // btn.disabled = false;
-  }, 1000);
+  // let counter = 1;
+  // const getData = mydebouncing(function () {
+  //   console.log("debouncing", counter++);
+  //   // btn.disabled = false;
+  // }, 1000);
 
   
   //throtling
