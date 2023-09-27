ba59e7fc6a522b5e047dc5563bc687cdc5dab94b
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-09-27 23:44:02 +0530

    DOM creation

--- Day31- DOM Creation/app.js
+++ Day31- DOM Creation/app.js
@@ -4,13 +4,26 @@ const h1 = document.createElement("h1");
 document.body.appendChild(h1);
 h1.innerText = "My DOM Tree";
 
+const ul = document.createElement("ul");
+document.body.appendChild(ul);
+const li = document.createElement("li");
+ul.appendChild(li);
+li.innerText = "MS Dhoni";
 
-const ul= document.createElement("ul")
-document.body.appendChild(ul)
-const li = document.createElement("li")
-ul.appendChild(li)
+//modifying attributes
+li.setAttribute("id", "heading");
+// console.log(li.getAttribute('id'));
+// li.removeAttribute("id");
 
-li.innerText = 'MS Dhoni'
-// const script = document.createElement("script")
-// script.setAttribute()
-// document.body.appendChild(script)
+const head = document.querySelector("#h1");
+head.setAttribute("id", "heading");
+
+
+
+//script tag added to the body
+const src = "./app.js";
+const script = document.createElement("script");
+script.setAttribute("src", src);
+document.body.appendChild(script);
+
+
